19ab71956b290cad6c49535fafd5392b94e5b772
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2015-08-04 17:16:43 +0200

    Added support for docker

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM debian:jessie
+MAINTAINER Marcin Wójcik <mwojcik@future-processing.com>, Mateusz Książek <mksiazek@future-processing.com>
+
+RUN apt-get update && \
+    apt-get -yq --no-install-recommends install \
+        nodejs-legacy \
+        npm \
+        ruby \
+     && apt-get purge -y --auto-remove \
+     && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /data/larmo-webapp
+
+RUN gem install sass
+RUN npm install -g bower
+RUN npm install -g grunt-cli
+
+EXPOSE 8080
+
+CMD [ "bash","-c", "npm install --ignore-scripts; npm run-script dockerInstall" ]
--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
     ];
 
     grunt.initConfig({
-        'pkg': grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
         'uglify': {
             'js': {
                 'src': jsFiles,
@@ -52,14 +52,51 @@ module.exports = function(grunt) {
                 'files': ['styles/*.scss', 'styles/*/*.scss'],
                 'tasks': ['sass:dev']
             }
+        },
+        'nodemon': {
+            dev: {
+                script: 'index.js'
+            }
+        },
+        concurrent: {
+            dev: {
+                tasks   : ['nodemon', 'watch'],
+                options : {
+                    logConcurrentOutput: true
+                }
+            }
+        },
+        shell: {
+            createReleaseForProduction: {
+                command: function(version) {
+                    if(version === undefined) {
+                        var currentVersion = '<%= pkg.version %>'.split('.');
+                        currentVersion[2]++;
+
+                        version = currentVersion.join('.');
+                    }
+
+                    return 'git checkout -b release-' + version + ';' +
+                        'git add -f public/css/style.min.css public/js/application.min.js;' +
+                        'git commit -am \'Saved for release\'; ' +
+                        'git tag -a ' + version + ' -m \'Release ' + version + '\'; ' +
+                        'git checkout master; git branch -D release-' + version + '; ';
+                }
+            }
         }
     });
 
-    grunt.registerTask('default', ['build', 'watch']);
+    //grunt.registerTask('default', ['build', 'concurrent:dev']);
     grunt.registerTask('build', ['sass:dist', 'uglify', 'sass:dev', 'includeSource']);
+
+    grunt.registerTask('development', ['sass:dev', 'includeSource', 'concurrent:dev']);
+    grunt.registerTask('release', ['sass:dist', 'uglify', 'shell:createReleaseForProduction']);
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-include-source');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-shell');
 };
